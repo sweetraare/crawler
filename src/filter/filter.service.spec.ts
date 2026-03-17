6525429f329e7b165c58e0510e8ecdc55e2146c7
Alex <ajua_1696@hotmail.com>
2026-03-17 19:32:23 -0300

    feat(c04-tests): add tests for filter

--- src/filter/filter.service.spec.ts
+++ src/filter/filter.service.spec.ts
@@ -15,4 +15,19 @@ describe('FilterService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('should return 0 when title is empty', () => {
+    const testTitle = '';
+    expect(service.countWords(testTitle)).toBe(0);
+  });
+
+  it('should count only spaced words', () => {
+    const testTitle = 'This is - a self-explained example';
+    expect(service.countWords(testTitle)).toBe(5);
+  });
+
+  it('should count only spaced words', () => {
+    const testTitle = 'alex-javier ulloa-arevalo';
+    expect(service.countWords(testTitle)).toBe(2);
+  });
 });
