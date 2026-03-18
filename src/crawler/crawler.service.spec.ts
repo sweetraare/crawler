1557269b319348eecb62817555e5f0a91dcb71f5
Alex <ajua_1696@hotmail.com>
2026-03-17 21:01:18 -0300

    feat(c04-tests): add tests for entries

--- src/crawler/crawler.service.spec.ts
+++ src/crawler/crawler.service.spec.ts
@@ -2,68 +2,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { CrawlerService } from './crawler.service';
 import axios from 'axios';
 import { New } from 'src/common/interfaces/new.interface';
+import { mockCrawlHTML } from '../common/__mocks__/crawlHTML.mock';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
-
-const mockCrawlHTML = `
-<html>
-  <body>
-    <table>
-      <tr class="athing" id="1">
-        <td class="title">
-          <span class="rank">1.</span>
-          <span class="titleline">
-            <a href="https://alex.com">Long long long long text</a>
-          </span>
-        </td>
-      </tr>
-      <tr>
-        <td class="subtext">
-          <span class="score" id="score_47419525">1 point</span>
-          by
-          <a href="https://alex.com" class="hnuser">Alex Ulloa</a>
-          <span class="age" title="2026-03-17T22:59:59 1773788399">
-            <a href="https://alex.com">0 minutes ago</a>
-          </span>
-          <span id="unv_47419525"></span>
-          |
-          <a href="https://alex.com">hide</a>
-          |
-          <a href="https://alex.com" class="hnpast">past</a>
-          |
-          <a href="https://alex.com">discuss</a>
-        </td>
-      </tr>
-      <tr class="athing" id="2">
-        <td class="title">
-          <span class="rank">2.</span>
-          <span class="titleline"><a href="https://alex.com">short title</a></span
-          >
-        </td>
-      </tr>
-      <tr>
-        <td class="subtext">
-          <span class="subline">
-            <span class="score" id="score_47419294">20 points</span> by
-            <a href="https://alex.com" class="hnuser">Another Alex</a>
-            <span class="age" title="2026-03-17T22:36:03 1773786963">
-              <a href="https://alex.com">24 minutes ago</a>
-            </span>
-            <span id="unv_47419294"></span> |
-            <a href="https://alex.com">hide</a> |
-            <a
-              href="https://alex.com"
-              class="hnpast"
-              >past</a>
-            | <a href="https://alex.com">4&nbsp;comments</a></span>
-        </td>
-      </tr>
-    </table>
-  </body>
-</html>
-
-`;
 
 describe('CrawlerService', () => {
   let service: CrawlerService;
--- src/entries/entries.service.spec.ts
+++ src/entries/entries.service.spec.ts
@@ -1,18 +1,68 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EntriesService } from './entries.service';
+import { CrawlerService } from '../crawler/crawler.service';
+import { FilterService } from '../filter/filter.service';
+import { UserLogsService } from '../user-logs/user-logs.service';
+import { FilterType } from '../filter/dto/filter-query.dto';
 
 describe('EntriesService', () => {
   let service: EntriesService;
+  let crawlerService: CrawlerService;
+  let filterService: FilterService;
+  let userLogsService: UserLogsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [EntriesService],
+      providers: [
+        EntriesService,
+        {
+          provide: CrawlerService,
+          useValue: { fetchEntries: jest.fn().mockResolvedValue([]) },
+        },
+        {
+          provide: FilterService,
+          useValue: { filterNews: jest.fn().mockReturnValue([]) },
+        },
+        {
+          provide: UserLogsService,
+          useValue: { createLog: jest.fn() },
+        },
+      ],
     }).compile();
 
     service = module.get<EntriesService>(EntriesService);
+    userLogsService = module.get<UserLogsService>(UserLogsService);
+    crawlerService = module.get<CrawlerService>(CrawlerService);
+    filterService = module.get<FilterService>(FilterService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create log when all entries are requested', async () => {
+    await service.getAllEntries();
+    expect(userLogsService.createLog).toHaveBeenCalledTimes(1);
+  });
+
+  it('should create log when short entries are requested', async () => {
+    await service.getFilteredEntries({ filter: FilterType.short });
+    expect(userLogsService.createLog).toHaveBeenCalledTimes(1);
+  });
+
+  it('should create log when long entries are requested', async () => {
+    await service.getFilteredEntries({ filter: FilterType.long });
+    expect(userLogsService.createLog).toHaveBeenCalledTimes(1);
+  });
+
+  it('should fetch all entries', async () => {
+    await service.getAllEntries();
+    expect(crawlerService.fetchEntries).toHaveBeenCalledTimes(1);
+  });
+
+  it('should fetch filter entries', async () => {
+    await service.getFilteredEntries({ filter: FilterType.long });
+    expect(crawlerService.fetchEntries).toHaveBeenCalledTimes(1);
+    expect(filterService.filterNews).toHaveBeenCalledTimes(1);
   });
 });
